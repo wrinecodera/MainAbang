fb5dcc5e9b3b38fa4a47180fa4c78326900e5f51
Wrine Codera <wrpu.codera@swu.edu.ph>
2018-12-18 01:39:01 -0800

    filter

--- src/pages/filter/filter.ts
+++ src/pages/filter/filter.ts
@@ -9,13 +9,19 @@ import {ViewController} from 'ionic-angular';
   templateUrl: 'filter.html',
 })
 export class FilterPage {
-  
+  structure: any = {lower: 2999, upper: 20999};
   constructor(private viewCtrl: ViewController) {
   }
-
+  onChange(ev: any) {
+    console.log('Changed', ev);
+  }
 onClose(){
   this.viewCtrl.dismiss();
-
 }
+filter = {}
 
+logForm() {
+
+  console.log('this.filter');
+}
 }
--- www/build/0.js
+++ www/build/0.js
@@ -59,13 +59,21 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var FilterPage = /** @class */ (function () {
     function FilterPage(viewCtrl) {
         this.viewCtrl = viewCtrl;
+        this.structure = { lower: 2999, upper: 20999 };
+        this.filter = {};
     }
+    FilterPage.prototype.onChange = function (ev) {
+        console.log('Changed', ev);
+    };
     FilterPage.prototype.onClose = function () {
         this.viewCtrl.dismiss();
     };
+    FilterPage.prototype.logForm = function () {
+        console.log('this.filter');
+    };
     FilterPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-filter',template:/*ion-inline-start:"C:\Users\user\Desktop\abang\src\pages\filter\filter.html"*/'\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>filter</ion-title>\n      <ion-buttons end (click)="onClose()">\n        <button ion-button icon-end>\n          \n          <ion-icon name="close"></ion-icon>\n        </button>\n      </ion-buttons>\n      \n    \n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n  \n</ion-content>\n'/*ion-inline-end:"C:\Users\user\Desktop\abang\src\pages\filter\filter.html"*/,
+            selector: 'page-filter',template:/*ion-inline-start:"C:\Users\user\Desktop\abang\src\pages\filter\filter.html"*/'\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>filter</ion-title>\n      <ion-buttons end (click)="onClose()">\n        <button ion-button icon-end>\n          \n          <ion-icon name="close"></ion-icon>\n        </button>\n      </ion-buttons>\n      \n    \n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n  <form (ngSubmit)="logForm()">  \n  <ion-list>\n\n    <ion-item>\n      <ion-label>Free water</ion-label>\n      <ion-checkbox [(ngModel)]="filter.freewater" name="freewater"></ion-checkbox>\n    </ion-item>\n  \n    <ion-item>\n      <ion-label>With cr</ion-label>\n      <ion-checkbox [(ngModel)]="filter.withcr" name="withcr"></ion-checkbox>\n    </ion-item>\n  \n  \n    <ion-item>\n      <ion-label>No curfew</ion-label>\n      <ion-checkbox [(ngModel)]="filter.nocurfew" name="nocurfew"></ion-checkbox>\n    </ion-item>\n    \n    <ion-item>\n      <ion-label>Pets allowed</ion-label>\n      <ion-checkbox [(ngModel)]="filter.petsallowed" name="petsallowed"></ion-checkbox>\n    </ion-item>\n\n    <ion-item>\n      <ion-label>Onsite parking</ion-label>\n      <ion-checkbox [(ngModel)]="filter.onsiteparking" name="onsiteparking"></ion-checkbox>\n    </ion-item>\n\n    <ion-item>\n      <ion-label>Free electricity</ion-label>\n      <ion-checkbox [(ngModel)]="filter.freelectricity" name="freelectricity"></ion-checkbox>\n    </ion-item>\n\n    <ion-item>\n      <ion-label>In-unit laundry</ion-label>\n      <ion-checkbox [(ngModel)]="filter.in-unitlaundry" name="in-unitlaundry"></ion-checkbox>\n    </ion-item>\n\n    <ion-item>\n      <ion-label>Aircon</ion-label>\n      <ion-checkbox [(ngModel)]="filter.aircon" name="aircon"></ion-checkbox>\n    </ion-item>\n\n    <ion-item>\n      <ion-label>with CCTV</ion-label>\n      <ion-checkbox [(ngModel)]="filter.cctv" name="cctv"></ion-checkbox>\n    </ion-item>\n\n    <ion-item>\n      <ion-label>with security team</ion-label>\n      <ion-checkbox [(ngModel)]="filter.security" name="security"></ion-checkbox>\n    </ion-item>\n  \n     \n  </ion-list>\n  \n<ion-list>\n\n  <ion-list-header>\n    Structure\n    <ion-badge item-end color="dark">php{{structure.lower}}</ion-badge>\n    <ion-badge item-end color="dark">php{{structure.upper}}</ion-badge>\n  </ion-list-header>\n  <ion-item>\n    <ion-range min="1000" max="30000" dualKnobs="true" pin="true" [(ngModel)]="structure" name="structure" (ionChange)="onChange($event)" color="dark">\n      <ion-label range-left>1000</ion-label>\n      <ion-label range-right>30000</ion-label>\n    </ion-range>\n  </ion-item>\n\n</ion-list>\n     <button ion-button class="btn" type="submit" block>Add Todo</button>\n    </form>\n\n  </ion-content>\n\n  '/*ion-inline-end:"C:\Users\user\Desktop\abang\src\pages\filter\filter.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["l" /* ViewController */]])
     ], FilterPage);
